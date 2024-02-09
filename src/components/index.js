7c57d64128fe8181e027438e64a37ea13a3c1441
Azarudhin <azarudhin425@gmail.com>
2024-02-09 15:09:20 +0530

    reves number

--- src/components/index.js
+++ src/components/index.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react'
 
 function Index() {
     const[isPara,setIsPara]=useState('')
-
+   
   return (
     <div style={{display:"flex",flexDirection:'column',justifyContent:'center',alignItems:'center',padding:'30px'}}>
       <input placeholder='Enter para' onChange={(e)=>setIsPara(e.target.value)} />
-  
         <h3>
             {isPara.split('').reverse()}
         </h3>
+       
+        <h3>Reversed Numbers :  {isPara
+    .split(' ')
+    .map(number => number.split('').reverse().join(''))
+    .join(' ')}</h3>
     </div>
   )
 }
